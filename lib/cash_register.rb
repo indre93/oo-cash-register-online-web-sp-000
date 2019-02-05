ff6a712eb28305c40cf7c6a25009fb9dafdaa9b9
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-05 23:40:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-    quantity.each {|item| items << title}
+    quantity.times {|item| items << title}
   end
 
   def apply_discount
